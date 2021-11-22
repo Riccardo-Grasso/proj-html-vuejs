403bd35b8bbda35f847e642593d534a11072f08e
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-22 18:32:43 +0100

    main section 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <main>
+    <!-- PRO BARBERS -->
+    <section class="pro-barber d-flex">
+      <div
+        class="container d-flex flex-column align-items-center padding-section"
+      >
+        <div class="on-title bolder fw-bold mb-1">AWARD WINNING</div>
+        <h2 class="title mb-4">Pro Barbers</h2>
+        <a class="proj-link mb-4" href="#">MEET YOUR NEW BARBER</a>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
