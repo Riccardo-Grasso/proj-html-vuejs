ba26975846836a6ac0bb2099488e5b651cfa6fc4
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-23 10:50:59 +0100

    main-finito

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -107,6 +107,29 @@
         </div>
       </div>
     </section>
+    <!-- BLOG POSTS -->
+    <section class="blog">
+      <div
+        class="container padding-section d-flex flex-column align-items-center"
+      >
+        <div class="on-title bolder fw-bold mb-1">
+          TALES FROM THE BARBER SHOP
+        </div>
+        <h2 class="section-title mb-4">Recent Blog Posts</h2>
+        <div class="blog-container row row-cols-3 pt-5 mb-4">
+          <div
+            v-for="(post, i) in postsList"
+            :key="i"
+            class="col d-flex flex-column align-items-center p-3"
+          >
+            <img :src="post.img" alt="post-img" class="mb-4 w-100" />
+            <h3 class="title mb-4 text-center">{{ post.title }}</h3>
+            <p class="paragraph text-center">{{ post.text }}</p>
+          </div>
+        </div>
+        <a class="proj-link" href="#">READ MORE FROM OUR BLOG</a>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -172,6 +195,23 @@ export default {
           text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos enim quia quasi? Suscipit quam quaerat nihil quo aspernatur consectetur, numquam ipsam, facere asperiores praesentium.",
           photo: require("@/assets/client-3.jpg"),
           name: "Mark Wilson",
+        },
+      ],
+      postsList: [
+        {
+          img: require("@/assets/blog1.jpg"),
+          title: "Avada Barbers Now Open",
+          text: "Avada Barbers are experts in the  licjety split trim and hair cut. Quick but careful and ridiculously good looking.",
+        },
+        {
+          img: require("@/assets/blog2.jpg"),
+          title: "Choosing The Right Barber",
+          text: "Take a seat in our fine leather chairs, lean back and let us lather you a fresh head in a luxurious fashion.",
+        },
+        {
+          img: require("@/assets/blog3.jpg"),
+          title: "Quick Service Guide",
+          text: "Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers",
         },
       ],
     };
